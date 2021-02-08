656e1b70617b35da67581ab9252307f0c3aa6368
Somak <somak.jha@gmail.com>
2021-02-08 21:31:26 +0530

    project started

---
+++ src/Components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Card.css';
+
+
+function Card({title,imageUrl,body}) {
+  return (
+   
+    <div className="card-container">
+        <div className="image-container">
+            <img src={imageUrl} alt=""/>
+        </div>
+        <div className="card-content">
+               <div className="card-title">
+                  <h3>{title}</h3>
+               </div>
+               <div className="card-body">
+                  <p><b>{body}</b></p> 
+               </div>
+        </div>
+        
+
+    </div>
+  );
+}
+
+export default Card;
