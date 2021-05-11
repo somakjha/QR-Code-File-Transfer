d873dabc3a7aa52392cf1d07d8426e0c8bee6bcf
somakjha <somak.jha@gmail.com>
2021-05-12 01:54:24 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+
 import React from 'react';
 import './App.css';
 import Card from './Components/Card';
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <Card
       title ="Scan QR To Upload"
-      imageUrl="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=uploadimagestorage.herokuapp.com/"
-      body="Below field shows the files in the queue"
+      imageUrl="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=uploadserverse.herokuapp.com/"
+      body="Download from below"
       />
     </div>
   )
--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
 import firebase from "../firebase";
+import './NameList.css';
 import React, { useState,useEffect } from 'react';
 var names=[];
+var i_name=[];
 function NameList()
  {    
       const [urls, seturls] = useState([]);
@@ -11,34 +12,39 @@ function NameList()
         var storageRef=storage.ref();
         var i=0;
         storageRef.listAll().then(function(result)
-        {
+        { 
           result.items.forEach(function(imageRef)
          {  
            i++;
+           //console.log(imageRef.name)
             displayImage(i,imageRef);
         });
   
         });
   
         function displayImage(row,images)
-        {
+        { 
           images.getDownloadURL().then(function(url){
           
             let urlx = [...urls];
             urlx.push(url);
             seturls(urlx);
             names.push(urlx);
+            i_name.push({[urlx]:images.name})
            
           });
         }
 
       }, []);
 
-       
-      
-     console.log(names.length)
-     var namelist=names.map((x) => <h6 key={x}><a key={x} href={x}>{x}</a></h6>);
-     return  (<div>{namelist}</div>); 
+   
+    var namelist=i_name.map((x) => <a style={{color: 'white'}} href={Object.keys(x)}><h6 key={Object.values(x)}>{Object.values(x)}</h6></a> );     
+     return  (
+      <div className="List-container">
+        {namelist}
+      </div>
+      )
+  
     
      
    
